b7ef9799ede0ecef9801f1b30f37f58b3d753238
mochfamir <mochamad.faishal@efishery.com>
2023-11-03 12:10:55 +0700

    feat(uas): init

--- screen/Home.js
+++ screen/Home.js
@@ -1,26 +1,99 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
-import { Text } from "react-native-paper";
-import { useCommonState } from "../context/CommonContext";
+import React, { useEffect, useState } from "react";
+import { View, FlatList, TouchableOpacity } from "react-native";
+import {
+  Card,
+  Title,
+  Paragraph,
+  ActivityIndicator,
+  MD2Colors,
+  Searchbar,
+} from "react-native-paper";
+import { faker } from "@faker-js/faker";
 
-const Home = () => {
-  const { state } = useCommonState();
+const fetchCityData = async () => {
+  try {
+    const response = await fetch(
+      "https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest"
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching city data:", error);
+    return [];
+  }
+};
+
+const Home = ({ navigation }) => {
+  const [cityData, setCityData] = useState([]);
+  const [searchQuery, setSearchQuery] = React.useState("");
+
+  const [filteredCities, setFilteredCities] = useState([]);
+
+  useEffect(() => {
+    fetchCityData()
+      .then(({ data }) => {
+        const listCity = data.map((datum) => ({
+          id: datum["ID State"],
+          name: datum["State"],
+          population: datum["Population"],
+          image: faker.image.urlLoremFlickr({ category: "city" }),
+        }));
+        setCityData(listCity);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setFilteredCities(cityData);
+    } else {
+      setFilteredCities(
+        [...cityData].filter((datum) =>
+          datum.name.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      );
+    }
+  }, [searchQuery, cityData]);
+
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity onPress={() => handleCityClick(item.name)}>
+        <Card style={{ margin: 16 }}>
+          <Card.Cover source={{ uri: item.image }} />
+          <Card.Content>
+            <Title>{item.name}</Title>
+            <Paragraph>Population: {item.population}</Paragraph>
+          </Card.Content>
+        </Card>
+      </TouchableOpacity>
+    );
+  };
+
+  const onChangeSearch = (query) => setSearchQuery(query);
+
+  const handleCityClick = (city) => {
+    navigation.navigate("Map", { city });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text variant='headlineMedium'>
-        Selamat Datang{" "}
-        {state?.nama === "Guest" || !state?.nama ? "" : state.nama}!
-      </Text>
+    <View>
+      <Searchbar
+        placeholder='Search'
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+        style={{ margin: 16 }}
+      />
+      {filteredCities.length > 0 ? (
+        <FlatList
+          data={filteredCities}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      ) : (
+        <ActivityIndicator animating={true} color={MD2Colors.red800} />
+      )}
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    marginHorizontal: 20,
-  },
-});
 
 export default Home;
--- screen/Map.js
+++ screen/Map.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { View, Text, StyleSheet } from "react-native";
 
-const Map = () => {
+const fetchLatLong = async (cityName) => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/geo/1.0/direct?q=${cityName},USA&limit=10&appid=fa0bfbc91520394356cdc94d3e7888c4`
+    );
+    const data = await response.json();
+    console.log(data);
+    return data.find((datum) => datum?.country === "US");
+  } catch (error) {
+    console.error("Error fetching city data:", error);
+    return [];
+  }
+};
+
+const Map = ({ route }) => {
+  const { city } = route.params;
+  const [cityData, setCityData] = useState({});
+
+  useEffect(() => {
+    fetchLatLong(city).then((data) => {
+      setCityData(data);
+    });
+  }, [city]);
+
   return (
     <View style={styles.container}>
       <Text>Mapsss</Text>
@@ -10,20 +33,22 @@ const Map = () => {
         provider={PROVIDER_GOOGLE}
         style={styles.map}
         initialRegion={{
-          latitude: -6.2017692, // Ganti dengan latitude awal
-          longitude: 106.7796261, // Ganti dengan longitude awal
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitude: 33.6505397,
+          longitude: -102.5118039,
+          // latitudeDelta: 1.922,
+          // longitudeDelta: 1.421,
         }}
       >
-        <Marker
-          coordinate={{
-            latitude: -6.201641160686483, // Ganti dengan latitude marker
-            longitude: 106.78222251263776, // Ganti dengan longitude marker
-          }}
-          title='Marker Title'
-          description='Marker Description'
-        />
+        {cityData?.lat && (
+          <Marker
+            coordinate={{
+              latitude: cityData?.lat,
+              longitude: cityData?.lon,
+            }}
+            title='Marker Title'
+            description='Marker Description'
+          />
+        )}
       </MapView>
     </View>
   );
